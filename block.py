2b759e1acc82fad73d86cb26c431d91431549fbc
Franz Guzmán <46214532+FranzGB@users.noreply.github.com>
2021-07-26 17:03:42 -0500

    Working platform

--- block.py
+++ block.py
@@ -11,7 +11,26 @@ def get_hash(prev_block):
     return hashlib.md5(content).hexdigest()
 
 def check_integrity():
-	files = sorted(os.listdir(BLOCKCHAIN_DIR), key
+    files = sorted(os.listdir(BLOCKCHAIN_DIR), key=lambda x: int(x))
+    
+    results = []
+
+    for file in files[1:]:
+        with open(BLOCKCHAIN_DIR + file) as f:
+            block =json.load(f)
+        
+        prev_hash = block.get('prev_block').get('hash')
+        prev_filename= block.get('prev_block').get('filename')
+        actual_hash = get_hash(prev_filename)
+
+        if prev_hash == actual_hash:
+            res = 'Ok'
+        else:
+            res = 'was Changed'
+        print(f'Block {prev_filename}: {res}')
+        results.append({'block': prev_filename,'results': res})
+        
+    return results
 
 
 def write_block(borrower, lender, amount):
@@ -29,7 +48,7 @@ def write_block(borrower, lender, amount):
         }
     }
     
-    current_block = BLOCKCHAIN_DIR + str(len(os.listdir(BLOCKCHAIN_DIR)) + 1)
+    current_block = BLOCKCHAIN_DIR + str(blocks_count + 1)
 
     with open(current_block, 'w') as f:
         json.dump(data, f, indent=4, ensure_ascii=False)
@@ -37,22 +56,9 @@ def write_block(borrower, lender, amount):
 
 
 def main():
-    write_block(borrower='Andrew', lender='Kate', amount=100)
+    #write_block(borrower='Andrew', lender='Kate', amount=100)
+    check_integrity()
 
 
 if __name__ == '__main__':
     main()
-
-import json
-import os
-import hashlib
-
-BLOCKCHAIN_DIR = 'blockchain/'
-
-def get_hash(prev_block):
-	with open(BLOCKCHAIN_DIR + prev_block, 'rb') as f:
-		content = f.read()
-	return hashlib.md5(content).hexdigest()
-
-def check_integrity():
-	files = sorted(os.listdir(BLOCKCHAIN_DIR), key
